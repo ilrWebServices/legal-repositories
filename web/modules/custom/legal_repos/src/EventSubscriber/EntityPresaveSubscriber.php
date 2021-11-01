b39a20c2ee8bc35b34671e549fa1a5b65fedb156
Jeff Amaral <jeff.amaral@cornell.edu>
2021-11-01 11:10:45 -0400

    Add automatic path aliases to consent decrees

--- web/modules/custom/legal_repos/src/EventSubscriber/EntityPresaveSubscriber.php
+++ web/modules/custom/legal_repos/src/EventSubscriber/EntityPresaveSubscriber.php
@@ -5,6 +5,7 @@ namespace Drupal\legal_repos\EventSubscriber;
 use Drupal\entity_events\Event\EntityEvent;
 use Drupal\entity_events\EventSubscriber\EntityEventPresaveSubscriber;
 use Drupal\node\NodeInterface;
+use Drupal\Core\Cache\Cache;
 
 class EntityPresaveSubscriber extends EntityEventPresaveSubscriber {
 
@@ -107,6 +108,18 @@ class EntityPresaveSubscriber extends EntityEventPresaveSubscriber {
       default:
         $entity->field_circuit = 'other';
     }
+
+    // Hard-code the path alias based on the case_number and version. Path auto
+    // is for the weak.
+    $clean_case_number = preg_replace('/[[:punct:]]/',' ', $entity->field_case_number->value);
+    $clean_case_number = preg_replace('/[ ]+/','-', trim($clean_case_number));
+    $calculated_alias = '/' . str_replace('_', '-', $entity->bundle()) . '/' . $clean_case_number . '-' . $entity->field_version_number->value;
+
+    $path_alias = $entity->path->first();
+    $path_alias->alias = $calculated_alias;
+
+    // Force cache invalidation for this node to pick up changes to the path.
+    Cache::invalidateTags($entity->getCacheTagsToInvalidate());
   }
 
 }
