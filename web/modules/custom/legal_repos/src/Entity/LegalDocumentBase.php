f2655102b5727f4ce1bf1b8f89c51c8422918ad1
Jeff Amaral <jeff.amaral@cornell.edu>
2022-03-04 14:34:24 -0500

    Move circuit determination from event subscriber to new bundle class base
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/legal_repos/src/Entity/LegalDocumentBase.php
+++ web/modules/custom/legal_repos/src/Entity/LegalDocumentBase.php
@@ -13,11 +13,13 @@ class LegalDocumentBase extends Node {
   public function preSave(EntityStorageInterface $storage) {
     parent::preSave($storage);
 
-    if ($this->field_resource_url->isEmpty()) {
-      return;
+    if (!$this->field_resource_url->isEmpty()) {
+      $this->field_resource_pdf_url = $this->getEcommonsPdfUrl($this->field_resource_url->uri);
     }
 
-    $this->field_resource_pdf_url = $this->getEcommonsPdfUrl($this->field_resource_url->uri);
+    if (!$this->field_state->isEmpty()) {
+      $this->field_circuit = $this->getCircuitForState($this->field_state->value);
+    }
   }
 
   /**
@@ -65,6 +67,81 @@ class LegalDocumentBase extends Node {
    */
   protected function httpClient() {
     return \Drupal::service('http_client');
+  }
+
+  protected function getCircuitForState(string $state): string {
+    switch ($state) {
+      case 'District of Columbia':
+        return 'District of Columbia';
+      case 'Maine':
+      case 'Massachusetts':
+      case 'New Hampshire':
+      case 'Rhode Island':
+      case 'Puerto Rico':
+        return '1st';
+      case 'Connecticut':
+      case 'New York':
+      case 'Vermont':
+        return '2nd';
+      case 'Delaware':
+      case 'New Jersey':
+      case 'Pennsylvania':
+      case 'Virgin Islands':
+        return '3rd';
+      case 'Maryland':
+      case 'North Carolina':
+      case 'South Carolina':
+      case 'Virginia':
+      case 'West Virginia':
+        return '4th';
+      case 'Louisiana':
+      case 'Mississippi':
+      case 'Texas':
+      case 'Canal Zone':
+        return '5th';
+      case 'Kentucky':
+      case 'Michigan':
+      case 'Ohio':
+      case 'Tennessee':
+        return '6th';
+      case 'Illinois':
+      case 'Indiana':
+      case 'Wisconsin':
+        return '7th';
+      case 'Arkansas':
+      case 'Iowa':
+      case 'Minnesota':
+      case 'Missouri':
+      case 'Nebraska':
+      case 'North Dakota':
+      case 'South Dakota':
+        return '8th';
+      case 'Alaska':
+      case 'Arizona':
+      case 'California':
+      case 'Idaho':
+      case 'Montana':
+      case 'Nevada':
+      case 'Oregon':
+      case 'Washington':
+      case 'Hawaii':
+      case 'Guam':
+      case 'Northern Mariana Islands':
+        return '9th';
+      case 'Colorado':
+      case 'Kansas':
+      case 'New Mexico':
+      case 'Oklahoma':
+      case 'Utah':
+      case 'Wyoming':
+        return '10th';
+      case 'Alabama':
+      case 'Florida':
+      case 'Georgia':
+        return '11th';
+      default:
+        return 'other';
+    }
   }
 
 }
